4610fb9270382e0f2796bbdbfef0558b2fd9c339
Luisa Katrina Reyes <luisakatrinareyes@gmail.com>
2020-02-27 17:09:48 +0800

    Data Access class

---
+++ LuisaKatrinaReyes.DSA.Midterm.VideoGames/LuisaKatrinaReyes.DSA.Midterm.VideoGames.Windows/DAL/DataAccess.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.Entity;
+
+namespace LuisaKatrinaReyes.DSA.Midterm.VideoGames.Windows.DAL
+{
+    class DataAccess : DbContext
+    {
+        public DataAccess() : base("myConnectionString")
+        {
+
+        }
+    }
+}
