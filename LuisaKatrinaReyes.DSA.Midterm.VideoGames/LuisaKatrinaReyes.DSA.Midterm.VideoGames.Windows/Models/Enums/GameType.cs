34b5dc857f6d012bb547f09898750b784cbb996c
Luisa Katrina Reyes <luisakatrinareyes@gmail.com>
2020-02-29 09:37:39 +0800

    Models and Enums

---
+++ LuisaKatrinaReyes.DSA.Midterm.VideoGames/LuisaKatrinaReyes.DSA.Midterm.VideoGames.Windows/Models/Enums/GameType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuisaKatrinaReyes.DSA.Midterm.VideoGames.Windows.Models.Enums
+{
+    public enum GameType
+    {
+     ActionAdventureGame=10,
+     ActionGame=10,
+     ModGame=10
+    }
+}
---
+++ LuisaKatrinaReyes.DSA.Midterm.VideoGames/LuisaKatrinaReyes.DSA.Midterm.VideoGames.Windows/Models/Enums/PointOfView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuisaKatrinaReyes.DSA.Midterm.VideoGames.Windows.Models.Enums
+{
+    public enum PointOfView
+    {
+        FirstPerson=10,
+        SecondPerson=10,
+        ThirdPerson=10
+    }
+}
